ae42c4fde7088e3876e27aba715edf9ff960322a
gerasantillan17 <gerapumas122@gmail.com>
2024-08-26 22:18:38 -0600

    Add files via upload

---
+++ EVA1_2_VARIABLES/JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author gerar
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println(100);
+        System.out.println('c' );
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println(1000.52);
+        
+        int edad;
+        double edad2;
+        String nombre;
+        String apellido;
+        double promedio;
+        
+        edad = 18;
+        System.out.println(edad);
+    }
+    
+}
